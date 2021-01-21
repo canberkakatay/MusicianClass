12c275e09140de19a295e62a0ce81639f8371ea3
Canberk akatay <canberk.akatay16@hotmail.com>
2021-01-22 02:13:11 +0300

    no message

--- MusicianClass/Musicians.swift
+++ MusicianClass/Musicians.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+enum MusicianType {
+    case LeadGuitar
+    case Vocalist
+    case Drummer
+    case Bassist
+    case Violenist
+}
+
+
+class Musicians {
+    
+    var name : String = ""
+    var age : Int = 0
+    var instrument : String = ""
+    var type : MusicianType
+    
+    init(nameInit: String,ageInit: Int,instrumentInit: String,typeInit: MusicianType) {
+       name = nameInit
+        age = ageInit
+        instrument = instrumentInit
+        type = typeInit
+        
+    }
+    
+     func sing() {
+        print("nothing else matters")
+    }
+    
+    private func test (){
+        print("test")
+    }
+    
+}
--- MusicianClass/SuperMusician.swift
+++ MusicianClass/SuperMusician.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+
+class SuperMusician : Musicians {
+    
+    func sing2(){
+        print("enter night")
+    }
+    
+    override func sing() {
+        super.sing()
+        print("exit light")
+    }
+    
+    
+}
